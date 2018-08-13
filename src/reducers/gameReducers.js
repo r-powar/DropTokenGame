a15cd862717635cc8c29c0879967582b98b54351
Raj Powar <raaj.powar@gmail.com>
2018-08-13 14:08:25 -0700

    initial redux setup

--- src/reducers/gameReducers.js
+++ src/reducers/gameReducers.js
@@ -1,3 +1,7 @@
 /**
  * Created by rpowar on 8/13/18.
  */
+
+export default (state = {}, action) => {
+    return{}
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,9 @@
 /**
  * Created by rpowar on 8/13/18.
  */
+import {combineReducers} from 'redux';
+import gameReducer from './gameReducers';
+
+export default combineReducers({
+    gameReducer
+});
